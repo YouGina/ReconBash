6fd6d9882ad7bc9b5acfd07132c7a0adcd4fcafb
YouGina <info@yougina.com>
2021-01-04 15:21:20 +0100

    Fix permission issue

--- call_amass.sh
+++ call_amass.sh
@@ -3,8 +3,9 @@ for dir in $(ls $TARGETS_PATH/); do
 	if ([ -z "$target" ] || [ $target = $dir ]); then
 		path="$TARGETS_PATH/$dir/"
 		cd $path
+		sudo cp domains /tmp/snap.amass/tmp/domains
 		if [ -f "domains" ]; then
-			amass enum -config "./amass_config.ini" -df domains -o amass_final
+			sudo amass enum -config "./amass_config.ini" -df /tmp/domains -o amass_final
 		fi
 		cat amass_final >> domains_final
 		python3 ${RECON_BASH_BASEPATH}/python/cleanup_for_scope.py domains_final
