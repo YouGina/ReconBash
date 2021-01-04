641d28a5b8fe33cafe9cea286a43fa647113c3a3
YouGina <info@yougina.com>
2021-01-04 15:28:44 +0100

    amass_config.ini path

--- call_amass.sh
+++ call_amass.sh
@@ -4,8 +4,10 @@ for dir in $(ls $TARGETS_PATH/); do
 		path="$TARGETS_PATH/$dir/"
 		cd $path
 		sudo cp domains /tmp/snap.amass/tmp/domains
+		sudo cp ${RECON_BASH_BASEPATH}/amass_config.ini /tmp/snap.amass/tmp/amass_config.ini
+		
 		if [ -f "domains" ]; then
-			sudo amass enum -config "${RECON_BASH_BASEPATH}/amass_config.ini" -df /tmp/domains -o amass_final
+			sudo amass enum -config /tmp/amass_config.ini -df /tmp/domains -o amass_final
 		fi
 		cat amass_final >> domains_final
 		python3 ${RECON_BASH_BASEPATH}/python/cleanup_for_scope.py domains_final
