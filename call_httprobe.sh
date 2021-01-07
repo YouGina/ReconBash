7bf2f18a2a77d78965dfdaeea9c807119c4c483c
YouGina <info@yougina.com>
2021-01-07 09:09:24 +0000

    Remove target check

--- call_httprobe.sh
+++ call_httprobe.sh
@@ -1,31 +1,29 @@
 target=$1
 for dir in $(ls $TARGETS_PATH/); do
 	if ([ -z "$target" ] || [ $target = $dir ]); then
-		if [ $dir != "geico" ]; then
-			path="$TARGETS_PATH/$dir/"
-			cd $path
-			if [ -f "domains_final" ]; then
-				if [ -f "masscan_remove_me" ]; then
-					echo "Masscan exists for $dir"
-					check=""
-					for p in $(cat masscan | awk -F' ' '{print $3}' | sort -u); do
-						check="$check -p http:$p -p https:$p"
-					done;
-					echo "Start running httprobe with $check"
-					cmd="httprobe -c 50 $check"
-					cat domains_final | shuf | $cmd | tee -a hosts_with_ports
-				else
-					cat domains_final | shuf | httprobe -c 40 -p xlarge | tee -a hosts_with_ports # hosts
-				fi
-				python3 ${RECON_BASH_BASEPATH}/python/cleanup_for_scope.py hosts
+		path="$TARGETS_PATH/$dir/"
+		cd $path
+		if [ -f "domains_final" ]; then
+			if [ -f "masscan_remove_me" ]; then
+				echo "Masscan exists for $dir"
+				check=""
+				for p in $(cat masscan | awk -F' ' '{print $3}' | sort -u); do
+					check="$check -p http:$p -p https:$p"
+				done;
+				echo "Start running httprobe with $check"
+				cmd="httprobe -c 50 $check"
+				cat domains_final | shuf | $cmd | tee -a hosts_with_ports
+			else
+				cat domains_final | shuf | httprobe -c 40 -p xlarge | tee -a hosts_with_ports # hosts
 			fi
-			# echo `pwd`
-			if [ -f "hosts" ]; then
-				sort -u -o hosts hosts
-			fi
-			if [ -f "hosts_with_ports" ]; then
-				sort -u -o hosts_with_ports hosts_with_ports
-			fi
+			python3 ${RECON_BASH_BASEPATH}/python/cleanup_for_scope.py hosts
+		fi
+		# echo `pwd`
+		if [ -f "hosts" ]; then
+			sort -u -o hosts hosts
+		fi
+		if [ -f "hosts_with_ports" ]; then
+			sort -u -o hosts_with_ports hosts_with_ports
 		fi
 	fi
 done
